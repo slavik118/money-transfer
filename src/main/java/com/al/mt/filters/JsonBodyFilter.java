1254451a25129c6e4f280798e43d3e90202066dc
hokushin118 <slavik118@mail.ru>
2019-10-01 23:55:20 +0300

    Refactored code

--- src/main/java/com/al/mt/filters/JsonBodyFilter.java
+++ src/main/java/com/al/mt/filters/JsonBodyFilter.java
@@ -11,31 +11,29 @@ import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
 import static spark.Spark.halt;
 import static com.al.mt.utils.JsonUtils.isJSONValid;
 
-/** 
- * Verifies if incoming body is not empty and in JSON format. 
+/**
+ * Verifies if incoming body is not empty and in JSON format.
  */
 public class JsonBodyFilter implements Filter {
-  private static final ImmutableSet<String> HTTP_METHODS_WITH_BODY = ImmutableSet.of("POST", "PUT");
+	private static final ImmutableSet<String> HTTP_METHODS_WITH_BODY = ImmutableSet.of("POST", "PUT");
 
-  private static void stopRequest(final String message) {
-    halt(HTTP_BAD_REQUEST, 
-    		
-    		APIResponse.builder()
-					.setStatus(Status.ERROR)
-					.setMessage(message)
-					.build()
-    		        .toJson());
-//    halt(HTTP_BAD_REQUEST, new APIResponse(Status.ERROR, message).toJson());
-  }
+	private static void stopRequest(final String message) {
+		halt(HTTP_BAD_REQUEST,
+				APIResponse.builder()
+				.setStatus(Status.ERROR)
+				.setMessage(message)
+				.build()
+				.toJson());
+	}
 
-  @Override
-  public void handle(final Request request, final Response response) {
-    if (HTTP_METHODS_WITH_BODY.contains(request.requestMethod())) {
-      if (request.body().isEmpty()) {
-        stopRequest("Body is empty");
-      } else if (!isJSONValid(request.body())) {
-        stopRequest("Body is required and it is not in JSON format");
-      }
-    }
-  }
+	@Override
+	public void handle(final Request request, final Response response) {
+		if (HTTP_METHODS_WITH_BODY.contains(request.requestMethod())) {
+			if (request.body().isEmpty()) {
+				stopRequest("Body is empty");
+			} else if (!isJSONValid(request.body())) {
+				stopRequest("Body is required and it is not in JSON format");
+			}
+		}
+	}
 }
